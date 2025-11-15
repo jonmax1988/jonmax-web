bef63580e725225a69b2978c6e50a9045e2f9e4a
max <hushunli3868@163.com>
2025-11-15 20:41:36 +0800

    整合security

--- jonmax-oa-admin/src/views/system/sysMenu/list.vue
+++ jonmax-oa-admin/src/views/system/sysMenu/list.vue
@@ -236,7 +236,7 @@ export default {
 
     // -------------
     add(row){
-      debugger
+      //debugger
       this.typeDisabled = false
       this.dialogTitle = '添加下级节点'
       this.dialogVisible = true
@@ -267,7 +267,7 @@ export default {
     },
 
     edit(row) {
-      debugger
+      //debugger
       this.dialogTitle = '修改节点'
       this.dialogVisible = true
 
